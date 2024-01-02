0dbe10260898b5d57637bc42c2e78d628babf5e7
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-01-02 11:43:44 -0700

    se eliminan los install y .htaccess

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <?php
-    $title = "Nosotros";
+    $title = "Inicio";
     $contacto = "active";
     include("includes/head.php");
 ?>
@@ -10,7 +10,7 @@
     <?php
     include("includes/preloader.php");
     include("includes/header.php");
-    include("modules/about.php");
+    include("modules/index.php");
     include("includes/footer.php");
     include("includes/scripts.php");
     ?>
